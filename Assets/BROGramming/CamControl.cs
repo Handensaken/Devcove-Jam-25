266a4d9534ec1dff289f19c2dbe881e8a23f4b3d
Handensaken <maxmax.larsson@gmail.com>
2025-04-26 17:04:34 +0200

    idk man

---
+++ Assets/BROGramming/CamControl.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CamControl : MonoBehaviour
+{
+    [SerializeField]
+    private CinemachineVirtualCamera fightCam;
+
+    [SerializeField]
+    private Transform CameraLocator;
+
+    [SerializeField]
+    private GameObject EndMarker;
+
+    void Start()
+    {
+        Debug.Log(GameEventManager.instance);
+        GameEventManager.instance.OnResumeCamControl += ToPlayer;
+    }
+
+    void OnDestroy()
+    {
+        GameEventManager.instance.OnResumeCamControl -= ToPlayer;
+    }
+
+    private void ToPlayer()
+    {
+        fightCam.gameObject.SetActive(false);
+        EndMarker.SetActive(false);
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (collision.gameObject.transform.position.x > transform.position.x)
+            {
+                fightCam.transform.position = CameraLocator.position;
+                fightCam.gameObject.SetActive(true);
+                GetComponent<BoxCollider2D>().isTrigger = false;
+                GameEventManager.instance.StopPlayerInput();
+            }
+        }
+    }
+}
--- Assets/BROGramming/PlayerController.cs
+++ Assets/BROGramming/PlayerController.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     //facingDir will be used to save the last known looking direction to orient the player sprite after moving
-   // private Vector2 facingDir;
+    // private Vector2 facingDir;
     //Denna bool moggar din vector2
     [HideInInspector]
     public bool isFacingRight;
@@ -15,7 +15,8 @@ public class PlayerController : MonoBehaviour
     //Reads the value from the input asset
     private Vector2 movementInput;
 
-    [SerializeField] Slider manaSlider;
+    [SerializeField]
+    Slider manaSlider;
 
     [SerializeField]
     private float movementSpeed = 5;
@@ -27,27 +28,47 @@ public class PlayerController : MonoBehaviour
     private SpriteRenderer playerSpriteRenderer;
 
     [Header("Brawler stuff")]
-    [SerializeField] GameObject fist;
-    [SerializeField] Transform fistHolder;
-    [SerializeField] float timeBetweenPunches;
+    [SerializeField]
+    GameObject fist;
+
+    [SerializeField]
+    Transform fistHolder;
+
+    [SerializeField]
+    float timeBetweenPunches;
 
     [Header("Mage stuff")]
-    [SerializeField] int MaxMana = 100;
+    [SerializeField]
+    int MaxMana = 100;
+
     [Tooltip("The minimum amount of mana needed to switch to mage")]
-    [SerializeField] int manaThreshhold = 10;
-    [SerializeField] GameObject fireball;
+    [SerializeField]
+    int manaThreshhold = 10;
+
+    [SerializeField]
+    GameObject fireball;
+
     [Tooltip("The minimum time it will take to shoot a spell")]
-    [SerializeField] float minChargeTime = 0.1f;
+    [SerializeField]
+    float minChargeTime = 0.1f;
+
     [Tooltip("The max time the spell will scale with")]
-    [SerializeField] float maxChargeTime = 1f;
+    [SerializeField]
+    float maxChargeTime = 1f;
+
     [Tooltip("The damage the spell will deal if shot at minimum charge")]
-    [SerializeField] float minDamage = 10;
+    [SerializeField]
+    float minDamage = 10;
+
     [Tooltip("The damage the spell will deal if the shot is fully charged")]
-    [SerializeField] float maxDamage = 100f;
+    [SerializeField]
+    float maxDamage = 100f;
 
-    [SerializeField] float manaCost = 25f;
+    [SerializeField]
+    float manaCost = 25f;
 
-    [SerializeField] float timeBetweenSpells;
+    [SerializeField]
+    float timeBetweenSpells;
 
     private float timeCharged = 0;
     private float timePassed = 0;
@@ -61,28 +82,51 @@ public class PlayerController : MonoBehaviour
     }
 
     private PlayerClass playerClass;
-    
+
+    private PlayerInput playerInput;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerInput = GetComponent<PlayerInput>();
         //Initiates the player with as a mage with max mana
         playerClass = PlayerClass.Mage;
         mana = MaxMana;
         playerSpriteRenderer = GetComponent<SpriteRenderer>();
+
+        Debug.Log(GameEventManager.instance);
+        GameEventManager.instance.OnForcedSwitch += SwitchPlayerClass;
+        GameEventManager.instance.OnActivatePlayerInput += ActivatePlayerInput;
+        GameEventManager.instance.OnStopPlayerInput += DisablePlayerInput;
     }
 
     //Set up player switch event. This could be done natively as is now, but later down the line we want effects and shit to be able to
     //hijack the event without needing 4 billion references here (cringe)
     void OnEnable()
     {
-        Debug.Log(GameEventManager.instance);
+        /*Debug.Log(GameEventManager.instance);
         GameEventManager.instance.OnForcedSwitch += SwitchPlayerClass;
+        GameEventManager.instance.OnActivatePlayerInput += ActivatePlayerInput;
+        GameEventManager.instance.OnStopPlayerInput += DisablePlayerInput;*/
+    }
+
+    void ActivatePlayerInput()
+    {
+        playerInput.actions.FindActionMap("Player").Enable();
+    }
+
+    void DisablePlayerInput()
+    {
+        playerInput.actions.FindActionMap("Player").Disable();
+
+        GetComponent<Animator>().SetTrigger("John Anime");
     }
 
     void OnDisable()
     {
         GameEventManager.instance.OnForcedSwitch -= SwitchPlayerClass;
+        GameEventManager.instance.OnActivatePlayerInput -= ActivatePlayerInput;
+        GameEventManager.instance.OnStopPlayerInput -= DisablePlayerInput;
     }
 
     // Update is called once per frame
@@ -119,27 +163,25 @@ public class PlayerController : MonoBehaviour
         movementInput = ctx.ReadValue<Vector2>();
         if (ctx.action.inProgress)
         {
-            
             if (movementInput.x >= 0)
             {
-
                 isFacingRight = true;
-                Debug.Log("true:" + isFacingRight);
             }
             else
             {
-
                 isFacingRight = false;
-                Debug.Log("false:" + isFacingRight);
             }
-
         }
     }
 
     public bool getfacingright()
     {
-        if (isFacingRight) return true; else return false;
+        if (isFacingRight)
+            return true;
+        else
+            return false;
     }
+
     //Reads attack input
     public void Attack(InputAction.CallbackContext ctx)
     {
@@ -157,12 +199,18 @@ public class PlayerController : MonoBehaviour
                     mana -= manaCost;
                     //TODO:Add support for charging attacks
                     //Here you could play an animation
-                    GameObject pog = Instantiate(fireball, fistHolder.transform.position, Quaternion.identity);
-                    if (isFacingRight) pog.GetComponent<spellBehaviour>().ShootRight(); else pog.GetComponent<spellBehaviour>().ShootLeft();
+                    GameObject pog = Instantiate(
+                        fireball,
+                        fistHolder.transform.position,
+                        Quaternion.identity
+                    );
+                    if (isFacingRight)
+                        pog.GetComponent<spellBehaviour>().ShootRight();
+                    else
+                        pog.GetComponent<spellBehaviour>().ShootLeft();
                     Debug.Log("Mage Attack");
                     timePassed = 0;
                 }
-               
             }
             else
             {
@@ -172,7 +220,6 @@ public class PlayerController : MonoBehaviour
                     Instantiate(fist, fistHolder.transform.position, Quaternion.identity);
                     timePassed = 0;
                 }
-               
             }
         }
 
@@ -236,6 +283,7 @@ public class PlayerController : MonoBehaviour
     public void RecieveMana(float manaRecieved)
     {
         mana += manaRecieved;
-        if(mana > MaxMana) mana = MaxMana;
+        if (mana > MaxMana)
+            mana = MaxMana;
     }
 }
