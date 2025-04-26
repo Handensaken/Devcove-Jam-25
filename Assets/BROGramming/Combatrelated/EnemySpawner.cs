89833facfdda1e43eccc947cafa8dd68a863f506
Raggebus <65668855+Raggebus@users.noreply.github.com>
2025-04-26 17:24:51 +0200

    Spawner should work

---
+++ Assets/BROGramming/Combatrelated/EnemySpawner.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public List<int> EnemyWaveCount = new List<int>();
+    [SerializeField] GameObject enemy;
+    [SerializeField] List<Transform> spawnlocations = new List<Transform>();
+
+    private bool shouldSpawn = false;
+   
+    private int currentwave = 0;
+    private int enemiesAlive = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldSpawn)
+        {
+            for (int i = 0; i < EnemyWaveCount[currentwave]; i++)
+            {
+                SpawnEnemy();
+            }
+            shouldSpawn = false;
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        int spawnplace = Random.Range(0, spawnlocations.Count);
+        Instantiate(enemy, spawnlocations[spawnplace].position, Quaternion.identity);
+        enemiesAlive++;
+        
+    }
+
+    public void RemoveEnemy()
+    {
+        enemiesAlive--;
+        if (enemiesAlive == 0 && !shouldSpawn)
+        {
+            if (currentwave < EnemyWaveCount.Count)
+            {
+                NewWave();
+            } else
+            {
+                //Här kan man skicka ut ett event att rummet är klart
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void NewWave()
+    {
+        currentwave++;
+        shouldSpawn = true;
+       
+    }
+
+    //Denna måste kallas för att starta igång rummet
+    public void Activate()
+    {
+        shouldSpawn = true;
+    }
+}
